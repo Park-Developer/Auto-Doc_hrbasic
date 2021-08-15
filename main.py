ad67716ceccb481b0ace9d8732e90b4470dbb8b9
Park-Developer <parkwonho94@gmail.com>
2021-08-15 13:57:51 +0900

    1차 테스트 버전

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon,QFont
 import HTML_Generator as html_ge
 import JobReader as job_re
 import raw_func
@@ -14,33 +14,50 @@ class MyApp(QWidget):
         self.setWindowTitle("AutoDoc")
         self.setGeometry(300,200,300,300)
         self.setWindowIcon(QIcon("autojob_logo.png"))
-        # Intro Label Setting
-        intro_label=QLabel("Comment Documentation Program")
 
-        intro_layout = QHBoxLayout()
-        intro_layout.addWidget(intro_label)
+        # Intro Label Setting
+        self.intro_label=QLabel("Comment Documentation Program")
+
+        self.intro_layout = QHBoxLayout()
+        self.intro_layout.addWidget(self.intro_label)
+
+        # Progress Label Setting
+        self.progress_label=QLabel("◼ Job Directory : ")
+        myFont =QFont()
+        myFont.setBold(True)
+        self.progress_label.setFont(myFont)
+
+        self.progress_label__address=QLabel("")
+        self.progress_layout = QHBoxLayout()
+        self.progress_layout.addWidget(self.progress_label)
+        self.progress_layout.addWidget(self.progress_label__address)
 
         # Button Setting
-        upload_Btn=QPushButton("Upload",self)
-        upload_Btn.clicked.connect(self.upload_clicked)
+        self.upload_Btn=QPushButton("Upload",self)
+        self.upload_Btn.clicked.connect(self.upload_clicked)
 
-        cancel_Btn=QPushButton("Cancel",self)
-        cancel_Btn.clicked.connect(self.cancel_clicked)
+        self.cancel_Btn=QPushButton("Cancel",self)
+        self.cancel_Btn.clicked.connect(self.cancel_clicked)
 
+        self.convert_Btn=QPushButton("Convert",self)
+        self.convert_Btn.clicked.connect(self.convert_clicked)
+        self.convert_Btn.setDisabled(True)
 
-        button_layout = QHBoxLayout()
-        button_layout.addStretch(1)
-        button_layout.addWidget(upload_Btn)
-        button_layout.addWidget(cancel_Btn)
-        button_layout.addStretch(1)
+        self.button_layout = QHBoxLayout()
+        self.button_layout.addStretch(1)
+        self.button_layout.addWidget(self.upload_Btn)
+        self.button_layout.addWidget(self.cancel_Btn)
+        self.button_layout.addWidget(self.convert_Btn)
+        self.button_layout.addStretch(1)
 
-        main_layout = QVBoxLayout()
-        main_layout.addLayout(intro_layout)
-        main_layout.addStretch(1)
-        main_layout.addLayout(button_layout )
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addLayout(self.intro_layout)
+        self.main_layout.addStretch(1)
+        self.main_layout.addLayout(self.progress_layout)
+        self.main_layout.addStretch(3)
+        self.main_layout.addLayout(self.button_layout)
 
-
-        self.setLayout(main_layout)
+        self.setLayout(self.main_layout)
 
         self.show()
 
@@ -49,11 +66,16 @@ class MyApp(QWidget):
         if (self.job_directory==""):
             pass
         else:
-            self.make_docHTML(self.job_directory);
-            QMessageBox.about(self,"message","Complete!!")
-            self.close()
+            QMessageBox.about(self,"message","Upload Complete!!")
+            self.convert_Btn.setDisabled(False)
+            self.progress_label__address.setText(self.job_directory)
             
     def cancel_clicked(self):
+        self.close()
+
+    def convert_clicked(self):
+        self.make_docHTML(self.job_directory)
+        QMessageBox.about(self, "message", "Conversion Complete!!")
         self.close()
 
     def make_docHTML(self,job_address):
